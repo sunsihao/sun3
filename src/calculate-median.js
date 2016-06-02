2065e588415bbcd57030eb22128917f94f01cd48
yangyue <m18710434142_1@163.com>
2016-06-02 16:40:44 +0800

    add first

---
+++ src/calculate-median.js
@@ -0,0 +1,23 @@
+function calculate_median(arr) {
+    function test(arr){
+        var ouArr = [];
+        for(var i=0; i<arr.length; i++){
+            if(i%2 == 0){
+                continue;
+            }
+            ouArr.push(arr[i]);
+        }
+        ouArr.sort(function(a, b){
+            return a - b;
+        });
+        if(ouArr.length%2 == 0){
+            var maxNum = ouArr[ouArr.length/2];
+            var minNum = ouArr[ouArr.length/2 - 1];
+            return (maxNum+minNum)/2;
+        }else{
+            return ouArr[Math.floor(ouArr.length/2)];
+        }
+    }
+}
+
+module.exports = calculate_median;
